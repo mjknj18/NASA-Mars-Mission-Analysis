e931a244a246f44cce42a1d1fc6cda6c17e576f0
Matt Kennedy <mjknj18@gmail.com>
2019-11-28 08:32:17 -0500

    Updated Flask App Code.

--- Mars_News_Analysis_App.py
+++ Mars_News_Analysis_App.py
@@ -9,8 +9,10 @@ import pymongo
 app = flask.Flask(__name__, template_folder = 'C:/Users/mjknj/Desktop/UNCC/Homework/Web Scraping/NASA-Mars-Mission-Analysis/')
 app.config["DEBUG"] = True
 
-#Connect to MongoDB Database
+#Connect to Mongo Database
 mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+#Define Mongo Database Parameters
 mongo_db = mongo_client['mars_db']
 mongo_col = mongo_db['latest_info']
 
@@ -20,22 +22,29 @@ mongo_col = mongo_db['latest_info']
 #Define Function for Home Screen Content
 def home():
 
-    #Launch NASA Mars Mission Analysis Web Page
+    #Extract Scraped Data from Mongo Database
     for item in mongo_col.find():
         scraped_data = item
 
+    #Launch NASA Mars Mission Analysis Web Page
     return render_template('index.html', **scraped_data)
 
-#Define Path for Scaping New Data & Updating Web Page
+#Define Path for Scraping New Data & Updating Web Page
 @app.route('/scrape', methods=['GET'])
 
-#Define Function for Scaping New Data & Updating Web Page
+#Define Function for Scraping New Data & Updating Web Page
 def new_scrape():
+    
+    #Import Scraping Function
     import mars_news_scraping as mns
+
+    #Run Scraping Function
     scraped_data = mns.scrape()
 
+    #Insert Scraped Data into Mongo Database
     mongo_col.insert_one(scraped_data)
 
+    #Reload App Home Screen
     return redirect(url_for('home'))
 
 #Initialize Flask App
